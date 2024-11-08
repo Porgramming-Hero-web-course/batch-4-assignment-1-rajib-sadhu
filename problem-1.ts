bb00bc08c90f9c1434e7a1664e2e6189d361cfaa
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2024-11-08 20:48:16 +0530

    problem 2 done

--- problem-1.ts
+++ problem-1.ts
@@ -2,13 +2,14 @@
   // Problem 1
 
   const sumArray = (numbers: number[]): number => {
+    // use reduce method
     return numbers.reduce((elem: number, num: number): number => {
       return elem + num;
     }, 0);
   };
 
   const result: number = sumArray([1, 2, 3, 4, 5]);
-  console.log(result);
+  //   console.log(result);
 
   //
 }
---
+++ problem-2.ts
@@ -0,0 +1,25 @@
+{
+  // Problem 2
+
+  const removeDuplicates = (numbers: number[]): number[] => {
+    const unique: number[] = []; //get a empty array first
+    // use while loop
+    let i: number = 0;
+    while (i < (numbers.length as number)) {
+      // check the previous number already in the unique[] or not, if not then push that number
+      if (!unique.includes(numbers[i])) {
+        unique.push(numbers[i]); //push the unique number, which is don's set in the unique array
+      }
+
+      i++;
+    }
+    return unique;
+  };
+
+  const result: number[] = removeDuplicates([
+    1, 2, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8,
+  ]);
+
+  // console.log(result)
+  //
+}
